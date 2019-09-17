ecc6d57f371f598b6a67621fbd70e02785f47428
Michelle Y Ho <michelle.y.ho@gmail.com>
2019-09-16 20:48:49 -0700

    add message flashing to newMenuItem

--- vagrant/restaurant_menu/project.py
+++ vagrant/restaurant_menu/project.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 app = Flask(__name__)
 
 #sqlalchemy database access
@@ -27,6 +27,7 @@ def newMenuItem(restaurant_id):
         newItem = MenuItem(name=request.form['name'], restaurant_id = restaurant_id)
         session.add(newItem)
         session.commit()
+        flash("New menu Item (%s)  created!"%newItem.name)
         return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
@@ -61,6 +62,7 @@ def deleteMenuItem(restaurant_id, menu_id):
 
 
 if __name__ == "__main__":
+    app.secret_key='super_secret_key'
     app.debug = True
     app.run(host = '0.0.0.0', port = 5000)
 
