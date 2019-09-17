d4159e75bff2347a1fcd446d93e752335021f38c
Michelle Y Ho <michelle.y.ho@gmail.com>
2019-09-16 20:09:39 -0700

    Add ability to edit current menu items, abilit to cancel as well

--- vagrant/restaurant_menu/project.py
+++ vagrant/restaurant_menu/project.py
@@ -21,6 +21,8 @@ def restaurantMenu(restaurant_id):
 
 @app.route('/restaurants/<int:restaurant_id>/new/', methods=['GET','POST'])
 def newMenuItem(restaurant_id):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
     if request.method == 'POST':
         newItem = MenuItem(name=request.form['name'], restaurant_id = restaurant_id)
         session.add(newItem)
@@ -29,19 +31,24 @@ def newMenuItem(restaurant_id):
     else:
         return render_template('newmenuitem.html', restaurant_id=restaurant_id)
 
-
-# Task 2: Create route for editMenuItem function here
-
-
-@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/')
+@app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/edit/', methods=['GET', 'POST'])
 def editMenuItem(restaurant_id, menu_id):
-    return "page to edit a menu item. Task 2 complete!"
-
-# Task 3: Create a route for deleteMenuItem function here
-
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
+    editedItem = session.query(MenuItem).filter_by(id=menu_id).one()
+    if request.method == 'POST':
+        if request.form['name']:
+            editedItem.name = request.form['name']
+        session.add(editedItem)
+        session.commit()
+        return redirect(url_for('restaurantMenu', restaurant_id=restaurant_id))
+    else:
+        return render_template('editmenuitem.html', restaurant_id=restaurant_id, menu_id=menu_id, item=editedItem)
 
 @app.route('/restaurants/<int:restaurant_id>/<int:menu_id>/delete/')
 def deleteMenuItem(restaurant_id, menu_id):
+    DBSession = sessionmaker(bind=engine)
+    session = DBSession()
     return "page to delete a menu item. Task 3 complete!"
 
 
